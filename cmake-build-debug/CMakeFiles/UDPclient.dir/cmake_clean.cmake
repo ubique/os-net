7ae899c2415fe66990bb96e7174f3cf79181a2b9
Domonion <artemkononov2@gmail.com>
2019-05-19 23:58:16 +0300

    hw5

---
+++ cmake-build-debug/CMakeFiles/UDPclient.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UDPclient.dir/UDPclient.cpp.o"
+  "CMakeFiles/UDPclient.dir/utils.cpp.o"
+  "UDPclient.pdb"
+  "UDPclient"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UDPclient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
