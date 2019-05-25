3539989686bf49f0c59e871dc51aa9e7d16c92e7
Arkady Siuris <arss860@gmail.com>
2019-05-25 17:04:04 +0300

    initial

---
+++ client.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <arpa/inet.h>
+#include <strings.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        std::cout << "usage : ./server addr port";
+        exit(EXIT_FAILURE);
+    }
+
+	std::cout << "Enter your name and mister Server will say hello to you!\n";
+	std::cout << "Enter stop if you want to stop!\n";
+
+    int port;
+    try {
+        port = std::stoi(argv[2]);
+    } catch (std::invalid_argument &e) {
+        std::cout << "Bad port, can't convert to int";
+        exit(EXIT_FAILURE);
+    }
+
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (fd == -1) {
+        perror("Can't create socket");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in server{};
+    server.sin_family = AF_INET;
+    server.sin_port = port;
+
+    if (inet_pton(AF_INET, argv[2], &server.sin_addr) == -1) {
+        perror("Bad adress");
+        exit(EXIT_FAILURE);
+    }
+
+    if (connect(fd, (sockaddr *) (&server), sizeof(server)) == -1) {
+        perror("Can't connect.");
+        exit(EXIT_FAILURE);
+    }
+
+    std::string message;
+    size_t const BUFFER_SIZE = 1000;
+    char buffer[BUFFER_SIZE];
+    bzero(buffer, BUFFER_SIZE);
+
+    bool working = true;
+    while (working) {
+        bzero(buffer, BUFFER_SIZE);
+        std::cin >> message;
+
+        if (message == "stop") {
+            working = false;
+        }
+
+        size_t sended = 0;
+        while (sended < message.size()) {
+            ssize_t curr_portion = send(fd, message.data(), message.length(), 0);
+            if (curr_portion == -1) {
+                perror("Can't send message.");
+                continue;
+            }
+            sended += curr_portion;
+        }
+
+        ssize_t len = recv(fd, buffer, BUFFER_SIZE, 0);
+        if (len == -1) {
+            perror("Can't read response./");
+            continue;
+        }
+
+        std::cout << buffer << '\n';
+    }
+
+    close(fd);
+}
---
+++ server.cpp
@@ -0,0 +1,102 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string>
+#include <iostream>
+#include <arpa/inet.h>
+#include <strings.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        std::cout << "usage : ./server addr port";
+        exit(EXIT_FAILURE);
+    }
+
+    int port;
+    try {
+        port = std::stoi(argv[2]);
+    } catch (std::invalid_argument &e) {
+        std::cout << "Bad port, can't convert to int";
+        exit(EXIT_FAILURE);
+    }
+
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (fd == -1) {
+        perror("Can't create socket");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in server{};
+    server.sin_family = AF_INET;
+    server.sin_port = port;
+
+    if (inet_pton(AF_INET, argv[2], &server.sin_addr) == -1) {
+        perror("Bad adress");
+        exit(EXIT_FAILURE);
+    }
+
+    if (bind(fd, (sockaddr *) &server, sizeof(server)) == -1) {
+        perror("Can't bind");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(fd, 10) == -1) {
+        perror("Can't listen");
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "Server ready!\n";
+
+    std::string hello = "hello ";
+    const size_t BUFFER_SIZE = 100;
+    char buffer[BUFFER_SIZE + hello.size()];
+
+    for (int i = 0; i < hello.size(); i++) {
+        buffer[i] = hello[i];
+    }
+
+    struct sockaddr_in client{};
+    socklen_t addrlen;
+
+    bool working = true;
+    while (working) {
+        int client_fd = accept(fd, (sockaddr *) &client, &addrlen);
+        if (client_fd == -1) {
+            perror("Can't accept");
+            continue;
+        }
+
+        std::cout << "Connection established!\n";
+
+        while (working) {
+            bzero(buffer + hello.size(), BUFFER_SIZE);
+            ssize_t len = recv(client_fd, buffer + hello.size(), BUFFER_SIZE, 0);
+            if (len == -1) {
+                perror("Can't receive a message");
+                continue;
+            }
+
+            if (len == 0) {
+                continue;
+            }
+
+            std::string message(buffer + hello.size(), buffer + hello.size() + len);
+            if (message == "stop") {
+                working = false;
+            }
+
+            int written = 0;
+            while (written < len + hello.size()) {
+                int currWrtitten = write(client_fd, buffer, len + hello.size());
+                if (currWrtitten == -1) {
+                    perror("Can't send message.");
+                    continue;
+                }
+                written += currWrtitten;
+            }
+        }
+    }
+    close(fd);
+}
