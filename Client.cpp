4b7ebbaff2e1b33b3c57eaea4bf0fff960ec51eb
Krotkov <krotkovd99@gmail.com>
2019-06-08 19:35:13 +0300

    Fix

--- Client.cpp
+++ Client.cpp
@@ -5,6 +5,7 @@
 #include <cstring>
 #include <arpa/inet.h>
 #include <vector>
+#include <iostream>
 #include "Client.h"
 
 Client::Client(char *clientAddress, uint16_t port) : socket(::socket(AF_INET, SOCK_STREAM, 0)) {
@@ -23,15 +24,25 @@ Client::Client(char *clientAddress, uint16_t port) : socket(::socket(AF_INET, SO
 }
 
 std::string Client::request(const std::string& text) {
-    if (send(socket, text.c_str(), text.length(), 0) != text.length()) {
-        throw client_exception("Сan't send request");
+    size_t write_size = 0;
+    while (write_size != text.length() + 1) {
+        ssize_t cur_write_size = ::send(socket, text.c_str() + write_size, text.length() + 1 - write_size, 0);
+        if (cur_write_size == -1) {
+            throw client_exception("Error while sending");
+        }
+        write_size += cur_write_size;
     }
+
     std::vector<char> response(SIZE);
-    int read = recv(socket, response.data(), SIZE, 0);
-    if (read == -1) {
-        throw client_exception("Can't read from file descriptor");
+    size_t read_size = 0;
+    while (read_size == 0 || response[read_size - 1] != '\0') {
+        ssize_t cur_read_size = recv(socket, response.data() + read_size, SIZE, 0);
+        if (cur_read_size == -1) {
+            throw client_exception("Error while reading");
+        }
+        read_size += cur_read_size;
     }
-    response.resize(read);
+    response.resize(read_size);
     return std::string(response.data());
 }
 
--- Server.cpp
+++ Server.cpp
@@ -38,25 +38,28 @@ void Server::run() {
         }
         std::cout << "Connected" << std::endl;
         while (true) {
-            std::vector<char> buffer(SIZE);
-            int read = recv(connection, buffer.data(), SIZE, 0);
-            if (read == -1) {
-                std::cerr << "Can't read info: " << strerror(errno) << std::endl;
-                continue;
-            } else if (read == 0) {
-                std::cout << "Client disconnected" << std::endl;
-                break;
-            } else {
-                while (send(connection, buffer.data(), read, 0) != read) {
-                    std::cerr << "Can't send info" << std::endl;
-                    std::cerr << "Retrying..." << std::endl;
+            std::vector<char> response(SIZE);
+            size_t read_size = 0;
+            while (read_size == 0 || response[read_size - 1] != '\0') {
+                ssize_t cur_read_size = ::recv(connection, response.data() + read_size, SIZE, 0);
+                if (cur_read_size == -1) {
+                    throw server_exception("Error while reading");
                 }
+                read_size += cur_read_size;
             }
-
+            response.resize(read_size);
+            size_t write_size = 0;
+            while (write_size != response.size()) {
+                ssize_t cur_write_size = ::send(connection, response.data() + write_size, response.size() - write_size, 0);
+                if (cur_write_size == -1) {
+                    throw server_exception("Error while sending");
+                }
+                write_size += cur_write_size;
+            }
         }
     }
 }
 
 #pragma clang diagnostic pop
 
-server_exception::server_exception(const std::string &message) : std::runtime_error(message) {}
+server_exception::server_exception(const std::string &message) : std::runtime_error(message + strerror(errno)) {}
