e15872e641d97dfa78c07006193039ca506a9404
RevealMind <99_dimas@mail.ru>
2019-06-09 05:30:08 +0300

    fixed some bugs

--- server.cpp
+++ server.cpp
@@ -1,97 +1,141 @@
 #include <iostream>
+#include <vector>
+#include <array>
 #include <unistd.h>
 #include <cstring>
+#include <string>
 #include <netinet/in.h>
 
-const int ATTEMPT = 5;
-const int BUFF_SIZE = 1024;
+const int BUFF_SIZE = 1025;
 const int PORT = 8080;
 
-void show_err(std::string const &error) {
-    fprintf(stderr, "ERROR: %s\n", error.c_str());
-}
-
 void show_usage() {
-    printf("Usage: [port]\n"
+    printf("Usage: server [port]\n"
            "by default port is 8080\n\n");
 }
 
-int open_socket() {
-    int listener = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (listener == -1) {
-        show_err("Cannot open socket");
-        exit(EXIT_FAILURE);
+int parse_input(int &port, char **in, int size) {
+    if (size > 2) {
+        std::cerr << "Too much arguments\n";
+        return -1;
     }
-
-    return listener;
+    try {
+        port = (size == 1) ? PORT : std::stoi(in[1]);
+    }
+    catch (std::exception const &e) {
+        perror("Incorrect port");
+        return -1;
+    }
+    return 0;
 }
 
-void start(int listener, int port) {
-    sockaddr_in serverAddress{};
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(port);
+// serever [port];
 
-    if (bind(listener, reinterpret_cast<sockaddr*>(&serverAddress), sizeof(sockaddr_in)) < 0) {
-        show_err("Cannot bind");
-        close(listener);
-        exit(EXIT_FAILURE);
-    }
+int main(int argc, char **argv) {
+    int port;
 
-    if (listen(listener, ATTEMPT) == -1) {
-        show_err("Cannot listen");
-        close(listener);
-        exit(EXIT_FAILURE);
-    }
-
-    for (int i = 0; i < ATTEMPT; ++i) {
-        sockaddr_in client{};
-        socklen_t clientSize;
-
-        int accepted_fd = accept(listener, reinterpret_cast<sockaddr*>(&client), &clientSize);
-        if (accepted_fd == -1) {
-            show_err("Cannot accept");
-            close(accepted_fd);
-            exit(EXIT_FAILURE);
-        }
-
-
-        char buffer[BUFF_SIZE] = {};
-
-        int readed = read(accepted_fd, buffer, BUFF_SIZE);
-        
-        if (readed == -1) {
-            show_err("Cannot read");
-            close(accepted_fd);
-            exit(EXIT_FAILURE);
-        }
-
-        if (send(accepted_fd, buffer, readed, 0) != readed) {
-            show_err("Cannot send");
-        }
-
-        close(accepted_fd);
-
-    }
-
-}
-
-int main(int argc, char** argv) {
-    if (argc > 2) {
+    if (parse_input(port, argv, argc) < 0) {
         show_usage();
-    }
-    int port = PORT;
-
-    if (argc == 2) {
-        port = atoi(argv[1]);
+        exit(1);
     }
 
-    int listener = open_socket();
+    std::cout << "Server started..." << std::endl;
 
-    start(listener, port);
+    int MasterSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (MasterSocket < 0) {
+        perror("Cannot open socket");
+        exit(2);
+    }
 
-    close(listener);
+    sockaddr_in SockAddr;
+    SockAddr.sin_family = AF_INET;
+    SockAddr.sin_port = htons(port);
+    SockAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    exit(EXIT_SUCCESS);
+    if (bind(MasterSocket, (struct sockaddr *)&SockAddr, sizeof(SockAddr)) < 0) {
+        perror("Cannot bind");
+        close(MasterSocket);
+        exit(3);
+    }
+
+    if (listen(MasterSocket, SOMAXCONN) < 0) {
+        perror("Cannot listen");
+        close(MasterSocket);
+        exit(4);
+    }
+
+
+    bool next = true;
+    while (true) {        
+        int SlaveSocket;
+
+        if (next) {
+            SlaveSocket = accept(MasterSocket, nullptr, nullptr);
+            if (SlaveSocket < 0) {
+            perror("Cannot accept");
+            shutdown(SlaveSocket, SHUT_RDWR);
+            close(SlaveSocket);
+            }
+        }
+
+        std::cout << "Receipt of a request..." << std::endl;
+
+        char buffer[BUFF_SIZE];  
+
+        next = false;
+        int read = 0;
+        std::string request = "";
+        do {            
+            read = recv (SlaveSocket, buffer, BUFF_SIZE, 0);
+            if (read <= 0) {
+                perror("Cannot receive request");
+                shutdown(SlaveSocket, SHUT_RDWR);
+                close(SlaveSocket);
+                next = true; 
+                break;
+            }    
+            request += std::string(buffer).substr(0, read);
+        } while (buffer[read - 1] != -1);
+        if (next) {
+            continue;
+        }
+
+
+        if (strcmp(request.substr(0, request.size() - 1).c_str(), "stop") == 0) {
+            std::cout << "Server stoped..." << std::endl;
+            std::cout << "Good Bye!" << std::endl;
+            shutdown(SlaveSocket, SHUT_RDWR);
+            close(SlaveSocket);
+            close(MasterSocket);
+            exit(EXIT_SUCCESS);
+        }
+
+        std::cout << "---------------------------------------------------------------------" << std::endl;
+        std::cout << "Received request: " << request.substr(0, request.size() - 1) << std::endl;
+        std::cout << "---------------------------------------------------------------------" << std::endl;
+
+
+        std::cout << "Sending a response..." << std::endl;
+        
+        std::string response;
+        int sent = 0;
+        next = false; 
+        do {
+            response = request.substr(sent, request.size());
+            int send_bytes = send(SlaveSocket, response.c_str(), response.size(), 0);
+            if (sent < 0) {
+                perror("Cannot send response");
+                shutdown(SlaveSocket, SHUT_RDWR);
+                close(SlaveSocket);
+                next = true;
+                break;
+            }
+            sent += send_bytes;
+        } while (sent < request.size());
+
+        if (next) 
+            continue;
+
+        std::cout << "Response sent!" << std::endl;
+    }
 }
