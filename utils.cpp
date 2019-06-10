5944f1f9c2485f990543d5aff1234658e8544f60
yaroslav <zhurba_ys@mail.ru>
2019-06-10 11:34:00 +0300

    utils fixed

--- utils.cpp
+++ utils.cpp
@@ -17,8 +17,8 @@ using std::string;
 void fun_send(char *what, int amount, int where, string message) {
     int counter = 0;
     while (counter < amount) {
-//        int sent = send(where, what + counter, amount - counter, MSG_NOSIGNAL);
-        int sent = send(where, what + counter, amount - counter, SO_NOSIGPIPE);
+        int sent = send(where, what + counter, amount - counter, MSG_NOSIGNAL);
+//        int sent = send(where, what + counter, amount - counter, SO_NOSIGPIPE);
         if (sent == -1) {
             cout << "error occurred with sent in " << message << "\n";
             cout << strerror(errno) << "\n";
@@ -31,8 +31,8 @@ void fun_send(char *what, int amount, int where, string message) {
 void fun_recv(char *where, int amount, int from, string message) {
     int counter = 0;
     while(counter < amount) {
-//        int received = recv(from, where + counter, amount - counter, MSG_NOSIGNAL);
-        int received = recv(from, where + counter, amount - counter, SO_NOSIGPIPE);
+        int received = recv(from, where + counter, amount - counter, MSG_NOSIGNAL);
+//        int received = recv(from, where + counter, amount - counter, SO_NOSIGPIPE);
         if (received == -1) {
             cout << "error occurred with recv "<< message << "\n";
             cout << strerror(errno) << "\n";
