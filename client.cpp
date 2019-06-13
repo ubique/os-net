839065f6a9ea0855ed4b9eb572f32bcd7128e9f1
vileme <wickdtarazed@gmail.com>
2019-06-14 00:41:52 +0300

    fixes

--- client.cpp
+++ client.cpp
@@ -21,22 +21,43 @@ int main(int argc , char* argv[]){
     addr.sin_family = AF_INET;
     addr.sin_port = htons (atoi(argv[1]));
     std::vector<char> response(BUFFER);
+    memset(response.data(),0,BUFFER);
     if(connect(clientSocket, ( struct sockaddr*) (&addr), sizeof(addr)) == -1){
         std::cerr<<"Error occured while creating the connection between socket and client"<<"\n";
         exit(EXIT_FAILURE);
     }
-    auto message = argv[2];
-    if(send(clientSocket,argv[2],strlen(argv[2]),0) == -1){
-        std::cerr<<"Error occured while sending the message to server"<<"\n";
-        exit(EXIT_FAILURE);
-    }
-    if(recv(clientSocket,response.data(),response.size(),0) == -1){
-        std::cerr<<"Error occured while receiving the message from server"<<"\n";
-        exit(EXIT_FAILURE);
-    }
-    std::cout<<"Result: "<<response.data()<<"\n";
-    if(strncmp(response.data(),"exit",response.size()) == 0){
-        std::cout<<"Quitting from the server"<<"\n";
+    std::string message = argv[2];
+    for(;;) {
+        size_t result = 0;
+        while (result < message.size()) {
+            int bytes = send(clientSocket, message.data() + result, message.size() - result, 0);
+            if (bytes == -1) {
+                std::cerr << "Error occured during sending the message";
+                exit(EXIT_FAILURE);
+            } else if (bytes == 0) {
+                break;
+            }
+            result += bytes;
+        }
+        // receiving msg itself
+        size_t sizeOfMessage = 0;
+        size_t receivedBytesMessage = 0;
+        while (receivedBytesMessage < message.size()) {
+            receivedBytesMessage = recv(clientSocket, response.data() + sizeOfMessage, response.size() - sizeOfMessage,
+                                        0);
+            if (receivedBytesMessage == -1) {
+                std::cerr << "Error occured during getting the message";
+                exit(EXIT_FAILURE);
+            }
+            sizeOfMessage += receivedBytesMessage;
+        }
+        std::cout << "Result: " << response.data() << "\n";
+        if (strncmp(response.data(), "exit", response.size()) == 0) {
+            std::cout << "Quitting from the server" << "\n";
+            break;
+        }
+        std::cin >> message;
+        memset(response.data(),0,BUFFER);
     }
     if(close(clientSocket) == -1){
         std::cerr<<"Error occured while closing the client socket"<<"\n";
--- server.cpp
+++ server.cpp
@@ -5,20 +5,20 @@
 #include <cstring>
 #include <unistd.h>
 
-constexpr  int BUFFER = 512;
+
+
+constexpr  int BUFFER = 1024;
 int main(int argc, char* argv[]) {
 
     if(argc != 2) {
         std::cerr << "Wrong format of input. Expected : [port]" << "\n";
         exit(EXIT_FAILURE);
     }
-
     int socket_descriptor;
     if((socket_descriptor = socket(AF_INET,SOCK_STREAM,0)) == -1) {
         std::cerr << "Error occured while creating the socket" << "\n";
         exit(EXIT_FAILURE);
     }
-
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_port = htons(atoi(argv[1]));
@@ -28,33 +28,54 @@ int main(int argc, char* argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    if(listen(socket_descriptor,1)==-1){
-        std::cerr<<"Error occured while setting listening a socker"<<"\n";
+    if(listen(socket_descriptor,1) == -1){
+        std::cerr<<"Error occured while setting listening a socket"<<"\n";
         exit(EXIT_FAILURE);
     }
     std::vector<char> response(BUFFER);
+    bool on = false;
     for(;;){
-
+        if(on) break;
+        memset(response.data(),0,BUFFER);
         int other;
         if((other = accept(socket_descriptor, nullptr,nullptr)) == -1){
             std::cerr<<"Error occured while receiving request from client";
             continue;
         }
-        ssize_t result;
-        if((result = recv(other,response.data(),BUFFER,0)) == -1 ){
-            std::cerr<<"Error ocuured while receiving a message"<<"\n";
-            continue;
-        }
-        if(strncmp(response.data(),"exit",result) == 0){
-            std::cout<<"Exiting the server... Wait for the free port before next use"<<"\n";
-            if(close(other) == -1){
-                std::cerr<<"Error occured while closing socket";
+        std::cout<<"Client is connected"<<"\n";
+        size_t receivedBytes = 0;
+        char data[BUFFER];
+        memset(data,0,BUFFER);
+        for(;;){
+            receivedBytes = recv(other,data,BUFFER,0);
+            if(receivedBytes == -1){
+                std::cerr<<"Error occured during receving the message of size : " << BUFFER << "\n";
+                continue;
             }
-            break;
-        }
-        if(send(other,response.data(),result,0) == -1){
-            std::cerr<<"Error occuted while sending the response"<<"\n";
-            continue;
+            if(receivedBytes == 0){
+                break;
+            }
+            size_t sizeOfMessage = 0;
+            size_t receivedBytesMessage = 0;
+            std::cout<<"Message(or part of it)\"" << data << "\" is received"<<"\n";
+            std::cout<<"Sending message to client...."<<"\n";
+            while(receivedBytesMessage < receivedBytes){
+                size_t sended = send(other,data + receivedBytesMessage, receivedBytes - receivedBytesMessage,0);
+                if(sended == -1){
+                    std::cerr<<"Error occured during getting the message";
+                    exit(EXIT_FAILURE);
+                }
+                receivedBytesMessage += sended;
+            }
+            if(strncmp(data,"exit",4) == 0){
+                std::cout<<"Exiting the server... Wait for the free port before next use"<<"\n";
+                if(close(other) == -1){
+                    std::cerr<<"Error occured while closing socket";
+                }
+                on = true;
+                break;
+            }
+            memset(data,0,BUFFER);
         }
     }
     if(close(socket_descriptor) == -1){
